c765dd9ceedf590ffea40b80a52ebd69a57b813d
jhonnycave <luis.j.cueva@gmail.com>
2022-10-31 17:21:33 -0500

    Updates

---
+++ precache-manifest.ff7c5e3f9533770b2ee44254a0b4d83f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9198b9454526c87f6c24122de1223f19",
+    "url": "/react_project_12/index.html"
+  },
+  {
+    "revision": "1248451dd9ef7a3fbab6",
+    "url": "/react_project_12/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4aa6c30de8d31b8988bd",
+    "url": "/react_project_12/static/js/2.059369df.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_project_12/static/js/2.059369df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1248451dd9ef7a3fbab6",
+    "url": "/react_project_12/static/js/main.16949d6b.chunk.js"
+  },
+  {
+    "revision": "d12ba930c456731636c1",
+    "url": "/react_project_12/static/js/runtime-main.d6ca7507.js"
+  }
+]);
